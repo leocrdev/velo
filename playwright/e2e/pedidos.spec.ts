c4db2f475b9a38deffe09e9893eafa8a9436ac5c
Leonardo Ribeiro <leocrdev@hotmail.com>
2026-01-21 22:09:28 -0300

    /// AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,24 +1,22 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
     await page.goto('http://localhost:5173/')
-
-    // Checkpoint 1
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido') // AAA - Arrange
 
-    // Checkpoint 2
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-    // Checkpoint 3  
+    // Act
     await page.getByTestId('search-order-id').fill('VLO-18DYA3')
-
     await page.getByTestId('search-order-button').click()
 
+    // Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible()
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-18DYA3')
-    
+
     await expect(page.getByTestId('order-result-status')).toBeVisible()
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
